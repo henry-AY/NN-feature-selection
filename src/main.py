ed2685eb62bfd7ff1f082f300ee6970c69779f66
henry-AY <henryyost402@gmail.com>
2026-03-08 14:14:34 -0700

    nearest neighbor completed

--- src/main.py
+++ src/main.py
@@ -19,28 +19,36 @@ def backward_elimination(data, features):
     pass
 
 def nearest_neighbor(data, features):
-    overall_score = 0
+    overall_preductions = 0
+    correct_predictions = 0
 
     for i in data:
         shortest_distance = sys.maxsize
+        neighbor = 0
 
         for j in data:
             if i != j:
-                # Somehow we only pick the selected features here
-                pass
+                distance = compute_distance(i, j, features)
 
+                if distance < shortest_distance:
+                    shortest_distance = distance
+                    neighbor = j
 
+        prediction = neighbor[0]
 
-                # Calculate euclid distance from i to j, and if its lower than best known score save it.
+        if prediction == i[0]:
+            correct_predictions += 1
+
+        overall_preductions += 1
 
         # Outside of the loop, we then predict based on the closest score saying it's a 1 or a 2. Then we see if we scored correct or not. 
         # We use this to calculate the overall_score. 
-    return
+    return correct_predictions / overall_preductions
 
 # https://www.khanacademy.org/math/geometry/hs-geo-analytic-geometry/hs-geo-distance-and-midpoints/a/distance-formula
 def compute_distance(i, j, features):
-    sum = 0;
-    for f in len(range(features)):
-        sum += (i[f] - j[f]) ** 2
+    distance_sum = 0
+    for f in features:
+        distance_sum += (i[f] - j[f]) ** 2
 
-    return math.sqrt(sum)
+    return math.sqrt(distance_sum)
