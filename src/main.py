64239c90fc4b84db7364faa8a24317cb49fcd359
henry-AY <henryyost402@gmail.com>
2026-03-08 14:01:16 -0700

    compute_distance implemented?

--- src/main.py
+++ src/main.py
@@ -1,3 +1,7 @@
+import sys
+import math
+
+
 def main():
     pass
 
@@ -7,14 +11,36 @@ def main():
 
 
 
-def forward_selection():
+def forward_selection(data, features):
     pass
 
 
-def backward_elimination():
+def backward_elimination(data, features):
     pass
 
-def nearest_neighbor():
-    pass
+def nearest_neighbor(data, features):
+    overall_score = 0
+
+    for i in data:
+        shortest_distance = sys.maxsize
+
+        for j in data:
+            if i != j:
+                # Somehow we only pick the selected features here
+                pass
 
 
+
+                # Calculate euclid distance from i to j, and if its lower than best known score save it.
+
+        # Outside of the loop, we then predict based on the closest score saying it's a 1 or a 2. Then we see if we scored correct or not. 
+        # We use this to calculate the overall_score. 
+    return
+
+# https://www.khanacademy.org/math/geometry/hs-geo-analytic-geometry/hs-geo-distance-and-midpoints/a/distance-formula
+def compute_distance(i, j, features):
+    sum = 0;
+    for f in len(range(features)):
+        sum += (i[f] - j[f]) ** 2
+
+    return math.sqrt(sum)
